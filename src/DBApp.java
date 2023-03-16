bd06d6f83b89eeaa79a941c1e57a3f8232f2b847
koka-afk <65974533+koka-afk@users.noreply.github.com>
2023-03-16 16:13:54 +0200

    Added CreateTable() method

--- src/DBApp.java
+++ src/DBApp.java
@@ -1,3 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
 public class DBApp {
-    
+
+    public void init(){};
+
+    public void createTable(String strTableName,
+                            String strClusteringKeyColumn,
+                            Hashtable<String,String> htblColNameType,
+                            Hashtable<String,String> htblColNameMin,
+                            Hashtable<String,String> htblColNameMax )
+            throws DBAppException, FileNotFoundException {
+        PrintWriter pw = new PrintWriter("meta-data.csv");
+        for(String name: htblColNameType.keySet()){
+            String type = htblColNameType.get(name);
+            if(!htblColNameMax.containsKey(name)) {
+                throw new DBAppException("No Such key in htbColNameMax");
+            }
+            if(!htblColNameMin.containsKey(name)){
+                throw new DBAppException("No Such key in htbColNameMin");
+            }
+            pw.print(strTableName + "," + name + "," + type + (strClusteringKeyColumn.equals(name)) + ",null,null," +
+                    htblColNameMin.get(name) + "," + htblColNameMax.get(name));
+        }
+
+    }
 }
---
+++ src/DBAppException.java
@@ -0,0 +1,8 @@
+public class DBAppException extends Exception{
+    public DBAppException(){
+        super();
+    }
+    public DBAppException(String errorMessage){
+        super(errorMessage);
+    }
+}
