df55e5aed0f307492071b37625ead4dfcd3283d6
koka-afk <65974533+koka-afk@users.noreply.github.com>
2023-03-16 18:03:33 +0200

    Finished verifying data inside insert method

--- src/DBApp.java
+++ src/DBApp.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 public class DBApp {
 
@@ -19,7 +21,7 @@ public class DBApp {
             if(!htblColNameMin.containsKey(name)){
                 throw new DBAppException("No Such key in htbColNameMin");
             }
-            pw.print(strTableName + "," + name + "," + type + "," + (strClusteringKeyColumn.equals(name)) + ",null,null," +
+            pw.println(strTableName + "," + name + "," + type + "," + (strClusteringKeyColumn.equals(name)) + ",null,null," +
                     htblColNameMin.get(name) + "," + htblColNameMax.get(name));
         }
         pw.close();
@@ -30,18 +32,53 @@ public class DBApp {
 
     }
 
+
+    public boolean checkTypeRange(Object o, String min, String max) throws ParseException {
+        if(o instanceof Integer){
+            int converted = (int) o;
+            int minConverted = Integer.parseInt(min), maxConverted = Integer.parseInt(max);
+            return converted >= minConverted && converted <= maxConverted;
+        }else if(o instanceof Double){
+            double converted = (double) o;
+            double minConverted = Double.parseDouble(min), maxConverted = Double.parseDouble(max);
+            return !(converted < minConverted) && !(converted > maxConverted);
+        }else if(o instanceof String){
+            String converted  = (String) o;
+            return converted.compareTo(min) >= 0 && converted.compareTo(max) <= 0;
+        }else {
+            Date converted = (Date) o;
+            Date minConverted = new SimpleDateFormat("YYYY-MM-DD").parse(min);
+            Date maxConverted = new SimpleDateFormat("YYYY-MM-DD").parse(max);
+            return converted.compareTo(minConverted) >= 0 && converted.compareTo(maxConverted) <= 0;
+        }
+    }
+
     public void insertIntoTable(String strTableName,
                                 Hashtable<String,Object> htblColNameValue)
-            throws DBAppException, IOException {
+            throws DBAppException, IOException, ParseException {
         boolean foundTable = false;
         Scanner sc = new Scanner(new FileReader("meta-data.csv"));
+        int cnt = 0;
         while(sc.hasNext()){
             String[] splitted = sc.next().split(",");
             if(splitted[0].equals(strTableName)){
+                cnt++;
                 foundTable = true;
-                //TODO validate data
-                break;
+                if(!htblColNameValue.containsKey(splitted[1])){
+                    throw new DBAppException("Table content does not match");
+                }
+                String type = htblColNameValue.get(splitted[1]).getClass().toString().substring(6);
+                if(!type.equals(splitted[2])){
+                    throw new DBAppException("Type Mismatch Error");
+                }
+                Object currentObject = htblColNameValue.get(splitted[1]);
+                if(!checkTypeRange(currentObject, splitted[6], splitted[7])){
+                    throw new DBAppException("Ranges Exceeded Error");
+                }
             }
+        }
+        if(cnt != htblColNameValue.size()){
+            throw new DBAppException("Input table does not match dimension of original table");
         }
         if(!foundTable){
             throw new DBAppException("No such table exist");
